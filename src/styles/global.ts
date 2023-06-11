47a21097c710eb60a6b6b4219387b0107feffc99
Bernardo De Marco Gonçalves <bernardomg2004@gmail.com>
2023-06-11 20:04:23 -0300

    added custom scroll bar

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -7,15 +7,6 @@ export const GlobalStyles = createGlobalStyle`
     padding: 0;
   }
 
-  :focus {
-    outline: 0;
-    box-shadow: none;
-  }
-
-  ::selection {
-    background-color: ${({ theme }) => theme['base-label']};
-  }
-
   body {
     background-color: ${({ theme }) => theme['base-background']};
     color: ${({ theme }) => theme['base-text']};
@@ -29,5 +20,34 @@ export const GlobalStyles = createGlobalStyle`
   h1, h2, h3, h4, h5, h6 {
     color: ${({ theme }) => theme['base-title']};
     font-weight: 700;
+  }
+
+  :focus {
+    outline: 0;
+    box-shadow: none;
+  }
+
+  ::selection {
+    background-color: ${({ theme }) => theme['base-label']};
+  }
+
+  body::-webkit-scrollbar {
+    width: 0.25rem;
+  }
+
+  body::-webkit-scrollbar-track {
+    background-color: ${({ theme }) => theme['base-background']};
+  }
+
+  body::-webkit-scrollbar-thumb {
+    background-color: ${({ theme }) => theme['base-span']};
+  }
+
+  @supports (scrollbar-color: red blue) {
+    body {
+      scrollbar-color: ${({ theme }) => theme['base-span']} ${({ theme }) =>
+  theme['base-background']};
+      scrollbar-width: auto;
+    }
   }
 `
